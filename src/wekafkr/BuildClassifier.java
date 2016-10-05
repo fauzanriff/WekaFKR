7078388314a58557fca3a59a4af7963b81cfb2b9
Khalil Ambiya <Khalil Ambiya>
2016-10-06 00:29:12 +0700

    draft Build Classifier

--- src/wekafkr/BuildClassifier.java
+++ src/wekafkr/BuildClassifier.java
@@ -5,10 +5,18 @@
  */
 package wekafkr;
 
+import weka.classifiers.trees.J48;
+import weka.core.Instances;
 /**
  *
  * @author User
  */
 public class BuildClassifier {
-    
+    public static void buildClassifier(Instances inst) throws Exception {
+        String[] options = new String[1];
+        options[0] = "-U";
+        J48 tree = new J48();
+        tree.setOptions(options);
+        tree.buildClassifier(inst);
+    }
 }
