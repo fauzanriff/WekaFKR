1f0df2091aac5813b1804b778fb6c9ab794e92a8
Rahman Adianto <rahman.adianto@gmail.com>
2016-10-06 08:08:39 +0700

    Implement entropy and Information Gain

--- src/wekafkr/classifier/MyID3.java
+++ src/wekafkr/classifier/MyID3.java
@@ -9,6 +9,7 @@ import weka.classifiers.Classifier;
 import weka.core.Instances;
 import weka.core.Attribute;
 import weka.core.Instance;
+import weka.core.Utils;
 
 /**
  *
@@ -44,5 +45,60 @@ public class MyID3 extends Classifier {
         this.instances.deleteWithMissingClass();
         
         //makeTree(this.instances);
+    }
+    
+    public double entropy(Instances instances) {
+        double entropy = 0;
+    
+        // Count number of instance in some class
+        double[] classCount = new double[instances.numClasses()];
+        for (int i = 0; i < instances.numInstances(); i++) {
+            int temp = (int) instances.instance(i).classValue();
+            classCount[temp]++;
+        }
+        
+        // Calculate entropy
+        for (int i = 0; i < instances.numClasses(); i++) {
+            if (classCount[i] > 0) {
+                double temp = classCount[i] / instances.numInstances();
+                entropy -= temp * Utils.log2(temp);
+            }
+        }
+        
+        return entropy;
+    }
+    
+    public double informationGain(Attribute attribute, Instances instances) {
+        double informationGain = entropy(instances);
+        
+        // Split data base on Anttribute
+        Instances[] splitDatabyAttribute = splitData(attribute, instances);
+        
+        // Calculate Information gain
+        for (int i = 0; i < splitDatabyAttribute.length; i++) {
+            if (splitDatabyAttribute[i].numInstances() > 0) {
+                double numInstancesAttribute = (double) splitDatabyAttribute[i].numInstances();
+                double numInstances = (double) instances.numInstances();
+                
+                informationGain -= (numInstancesAttribute / numInstances) * entropy(splitDatabyAttribute[i]);
+            }
+        }
+        
+        return informationGain;
+    }
+    
+     public Instances[] splitData(Attribute attribute, Instances instances) {
+        Instances[] split = new Instances[attribute.numValues()];
+        
+        for (int i = 0; i < attribute.numValues(); i++) {
+            split[i] = new Instances(instances, instances.numInstances());
+        }
+
+        for (int i = 0; i < instances.numInstances(); i++) {
+            int temp = (int) instances.instance(i).value(attribute);
+            split[temp].add(instances.instance(i));
+        }
+        
+        return split;
     }
 }
